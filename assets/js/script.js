033becd2bc1fc4e1147facd60671cf77f1dd72e5
Dotunj <dotunjolaosho@gmail.com>
2017-03-24 00:50:13 +0100

    JS stuff

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+window.onload=function();
+
+function get_todos(){
+	var todos = new Array;
+	var todos_str = localStorage.getItem('todo');
+	if(todos_str != null){
+		todos = JSON.parse(todos_str);
+	}
+   return todos;
+}
+ function add(){
+ 	var task = document.getElementById('text').value;
+
+    var todos = get_todos();
+    todos.push(task);
+    localStorage.setItem('todo', JSON.stringify(todos));
+
+    show();
+
+    return false;
+ }
